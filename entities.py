08535ac64b10d3d2c53a99f3a60a1c642644f4b2
jnthnrzr <jrozari00@citymail.cuny.edu>
2016-12-26 22:36:56 -0500

    add_stock

--- entities.py
+++ entities.py
@@ -2,16 +2,24 @@
 Defines the Investor and Broker classes"""
 
 
+class Portfolio(symbol, date):
+    """Portfolio class for stocks"""
+    symbol = symbol
+    date = date
+
+    
+
 class Investor(name, cash):
     """Investor class for anyone wishing to invest with initial cash injection"""
     cash = cash
     risk_money = 0.5 * cash
-    portfolio = []
+    portfolios = []
 
     def add_stock(symbol, quantity):
         """Adds stock of given symbol and quantity to the portfolio"""
-        stock_price_total = "SQL"   # TODO write sql statement
-        
+        stock_price_total = "1000"   # TODO write SQL statement
+        # TODO deduct stock quantity from market
+        portfolios.append(tuple(symbol, quantity, stock_price_total))
 
 class Broker(name, portfolio):
     """Broker class for faciliator of finances"""
