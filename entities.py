3a43756d59bdb25da9165afde3283aa4d614fc51
jnthnrzr <jrozari00@citymail.cuny.edu>
2016-12-26 22:46:54 -0500

    Added unit price in tuple of portfolios

--- entities.py
+++ entities.py
@@ -17,9 +17,10 @@ class Investor(name, cash):
 
     def add_stock(symbol, quantity):
         """Adds stock of given symbol and quantity to the portfolio"""
+        stock_price_unit = "10"   # TODO write SQL statement
         stock_price_total = "1000"   # TODO write SQL statement
         # TODO deduct stock quantity from market
-        portfolios.append(tuple(symbol, quantity, stock_price_total))
+        portfolios.append(tuple(symbol, quantity, stock_price_unit))
 
 class Broker(name, portfolio):
     """Broker class for faciliator of finances"""
