cd30f9f7e47ea69dfd992d25d97fd05aee8f6b67
Jonathan Rozario <jnthnrzr@users.noreply.github.com>
2016-12-28 01:13:42 -0500

    docstring

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
-"""Config.py
-Stores authentication info for database connection."""
+"""settings.py
+
+Stores authentication info for database connection.
+"""
 
 
 HOST = 'HOST'
 USER = 'USER'
 PASSWD = 'PASSWD'
 DB = 'DB'
-
