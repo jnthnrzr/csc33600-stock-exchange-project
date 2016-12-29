2940658b6cdd027694706f0311f30b20a10d6309
jnthnrzr <jrozari00@citymail.cuny.edu>
2016-12-28 21:28:30 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from entities import Portfolio, Investor, Broker
+
+
+all_investors = list()
+
+# Create short investors
+short1 = Investor("Shorty", 250000)
+short2 = Investor("Morty", 250000)
+
+# Create margin investors
+margin1 = Investor("Corky", 250000)
+margin2 = Investor("Dorky", 250000)
+
