2108e68bb53cd951776c9cc238c3c6ce64143100
jnthnrzr <jrozari00@citymail.cuny.edu>
2016-12-31 02:48:26 -0500

    short_selling complete?

--- short.py
+++ short.py
@@ -13,50 +13,55 @@ logging.basicConfig(level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
 # logging.disable(logging.CRITICAL)
 
-def give_stocks_short(investor, broker):
-    """Investor gets a random share from broker for short selling"""
 
-    logging.info("RUNNING give_stocks_short()...")
-    # Take a loan from broker to investor
-    loan = min(investor.credit_line, investor.risk_money)
-    investor.cash += loan
-    broker.cash -= loan
-    
-    # Pay broker fees
-    fee = broker.fee
-    investor.cash -= fee
-    broker.cash += fee
+def short_seller(investor, broker):
 
-    # Pick a stock from broker at random and buy it
-    sym, qty, price = random.choice(broker.portfolios[0].portfolios)
+    old_portfolio = investor.portfolios[0]
+    new_portfolio = investor.portfolios[1]
+    # logging.info("new_portfolio: %s" % new_portfolio)
+    # logging.info(new_portfolio.portfolios[0][2])
+    stock_price = Decimal(str(old_portfolio.portfolios[0][2]))
+    cur_stk_price = Decimal(str(new_portfolio.portfolios[0][2]))
+    stock_quant = old_portfolio.portfolios[0][1]
+    stock_symbol = old_portfolio.portfolios[0][0]
 
-    logging.info("Picked stock %s, qty %s, price %s" % (sym, qty, price))
+    # logging.info(old_portfolio.value)
+    # looking for decres in stock price 
+        
+    # If true we buy back the stock at a lose
+    max_loss = (0.5*((investor.cash)+float(old_portfolio.value)) - (investor.risk_money))
+    logging.info("short seller: %s" % investor)
 
-    # Buy correct quantity
-    qty_tobuy = int ( (investor.cash / Decimal(price)) )
-    logging.info( "qty %s to buy of %s" % (qty_tobuy, sym) )
 
-    logging.info("BUYING...")
+    if stock_price == cur_stk_price:
+        logging.info("stock_price %s is equal to cur_stk_price %s" % (stock_price, cur_stk_price))
+        return "stock_price is equal to cur_stk_price"
 
-    if (qty_tobuy >= qty):
-        first_purchase = qty * Decimal(price)
-        investor.cash -= first_purchase
-        logging.info("Buying all of %s" % sym)
+    elif (stock_price-cur_stk_price > 0):
+        investor.portfolios[0].remove_stock(stock_symbol, stock_quant)
+        investor.cash += float(old_portfolio.value)
+        # buy new portfolio
+        investor.portfolios[0].add_stock(stock_symbol, stock_quant, cur_stk_price)
+        investor.cash -= float(new_portfolio.value)
+        #investor.portfolios[0].remove_stock(stock_symbol)
+        #investor.cash = investor.cash + stock_total_value
+        #investor.portfolios[0].add_stock(stock_symbol,stock_quant,cur_stk_price)
+        #investor.cash = investor.cash - (cur_stk_price*stock_quant)
+        logging.info("stock bought back at a profit %s ... %s " % (stock_price, cur_stk_price))
+    elif ((stock_quant * cur_stk_price) >= max_loss):
+        # sale old portfolio, get cash
+        investor.portfolios[0].remove_stock(stock_symbol, stock_quant)
+        investor.cash += float(old_portfolio.value)
+        # buy new portfolio
+        investor.portfolios[0].add_stock(stock_symbol, stock_quant, cur_stk_price)
+        investor.cash -= float(new_portfolio.value)
+        logging.info("bought back at a lose %s .. %s" % (stock_price, cur_stk_price))
+
     else:
-        investor.cash -= qty_tobuy * price
-
-    logging.info("AFTER BUY")
-
-    # Make portfolio p and add stock to it
-    p = Portfolio()
-    p.add_stock(sym, qty, price)
-    investor.add_portfolio(p)
-    logging.info("stock %s, %s, %s has been added" % (sym, qty, price))
-    logging.info("Displaying investors: %s" % (investor))
-
-    # Remove stock from broker's portfolio
-    logging.info("Broker's portfolio BEROFE removal: %s" % (broker.portfolios[0].portfolios))
-    broker.portfolios[0].remove_stock(sym, qty)
-
-    logging.info("Broker portfolio AFTER removal: %s" % (broker.portfolios[0].portfolios))
+        logging.info("short selling didn't buy back stocks")
+       
     
+    #print "\n","trade.py",stock_symbol,invest_portf.portfolios, stock_total_value
+    #print "\n","current price of ",stock_symbol,cur_stk_price
+    
+
